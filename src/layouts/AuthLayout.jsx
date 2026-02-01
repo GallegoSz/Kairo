8b5ad18479682958f953e4447776f9a999208aa8
Joao Pedro Gallego de Souza <jopedrogallego@gmail.com>
2026-02-01 10:48:07 -0400

    A tela de login foi repaginada

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-gray-900 via-gray-800 to-orange-900">
       <Outlet />
     </div>
   )
